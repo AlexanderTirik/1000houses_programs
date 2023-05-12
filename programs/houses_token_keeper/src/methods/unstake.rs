c459d2d6d8a947ce00c321354833b69195d347b3
AlexanderTirik <sasha.tirik@gmail.com>
2023-05-12 19:55:21 -0400

    implement cpi unstake

---
+++ programs/houses_token_keeper/src/methods/unstake.rs
@@ -0,0 +1,80 @@
+use anchor_lang::{prelude::*};
+use anchor_spl::{token::{Mint, Token, TokenAccount}, associated_token::AssociatedToken};
+use houses_stake::cpi::accounts::Unstake as CpiUnstake;
+use houses_stake::types::{StakePda, Data};
+use houses_stake::program::HousesStake;
+
+use crate::types::*;
+
+pub fn unstake(ctx: Context<Unstake>, email: String, amount: u64
+) -> Result<()> {
+    let stake_program = ctx.accounts.stake_program.to_account_info();
+    let seeds = &[email.as_ref(), ctx.accounts.authority.key.as_ref(), &[*ctx.bumps.get("user_pda").unwrap()]];
+    let signer = &[&seeds[..]];
+    let cpi_accounts = CpiUnstake {
+        token_program: ctx.accounts.token_program.to_account_info(),
+        system_program: ctx.accounts.system_program.to_account_info(),
+        associated_token_program: ctx.accounts.associated_token_program.to_account_info(),
+        token_mint: ctx.accounts.token_mint.to_account_info(),
+        authority: ctx.accounts.authority.to_account_info(),
+        stake_pda: ctx.accounts.stake_pda.to_account_info(),
+        stake_pda_token_account: ctx.accounts.stake_pda_token_account.to_account_info(),
+        data_pda: ctx.accounts.data_pda.to_account_info(),
+        user_token_account: ctx.accounts.user_pda_token_account.to_account_info(),
+        user: ctx.accounts.user_pda.to_account_info(),
+    };
+    let cpi_ctx = CpiContext::new_with_signer(stake_program, cpi_accounts, signer);
+    houses_stake::cpi::unstake(cpi_ctx, email.to_owned(), amount)?;
+    Ok(())
+    }
+
+#[derive(Accounts)]
+#[instruction(email: String)]
+pub struct Unstake<'info> {
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    #[account(
+        address = STAKE_PROGRAM_ADDRESS.parse::<Pubkey>().unwrap(),
+    )]
+    pub stake_program: Program<'info, HousesStake>,
+
+    #[account(mut,
+        address = TOKEN_MINT_ADDRESS.parse::<Pubkey>().unwrap(),
+    )]
+    pub token_mint: Account<'info, Mint>,
+
+    #[account(init_if_needed,
+        space = 8 + 1,
+        payer = authority,
+        // constraint rethink
+        seeds = [ email.as_ref(), authority.key.as_ref() ],
+        bump)]
+    pub user_pda: Account<'info, UserPda>,
+
+    #[account(mut,
+        associated_token::mint = token_mint,
+        associated_token::authority = user_pda
+    )]
+    pub user_pda_token_account: Account<'info, TokenAccount>,
+
+    #[account()]
+    pub stake_pda: Account<'info, StakePda>,
+
+
+    #[account(
+        init_if_needed,
+        payer = authority, 
+        associated_token::mint = token_mint,
+        associated_token::authority = stake_pda
+    )]
+    pub stake_pda_token_account: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    pub data_pda: Account<'info, Data>,
+
+    #[account(mut,
+        address = AUTHORITY_ADDRESS.parse::<Pubkey>().unwrap(),
+    )]
+    pub authority: Signer<'info>,
+}
---
+++ tests/houses_token_keeper/unstake.ts
@@ -0,0 +1,114 @@
+import * as anchor from '@coral-xyz/anchor';
+import { BN, Program } from '@coral-xyz/anchor';
+import { HousesTokenKeeper } from '../../target/types/houses_token_keeper';
+import { getKeypairFromFile } from '../../utils/getKeypairFromFile';
+import { requestAirdrop } from '../../helpers/requestAirdrop';
+import { PublicKey } from '@solana/web3.js';
+import {
+  TOKEN_PROGRAM_ID,
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  mintTo,
+  getOrCreateAssociatedTokenAccount,
+} from '@solana/spl-token';
+import { getLocalMint } from '../../helpers/getLocalMint';
+import { getTokenKeeperAccounts } from '../../helpers/houses_token_keeper/getTokenKeeperAccounts';
+import { getStakeAccounts } from '../../helpers/houses_stake/getStakeAccounts';
+import { HousesStake } from '../../target/types/houses_stake';
+import { getRandomEmail } from '../../utils/getRandomEmail';
+import { assert } from 'chai';
+import { getTokenAmount } from '../../helpers/getTokenAmount';
+import { cpiStake } from './stake';
+
+export const cpiUnstake = async (email, amount) => {
+  const program = anchor.workspace
+    .HousesTokenKeeper as Program<HousesTokenKeeper>;
+  const connection = program.provider.connection;
+
+  const { userPda, userPdaTokenAccount } = await getTokenKeeperAccounts(email);
+  const [stakePda] = PublicKey.findProgramAddressSync(
+    [Buffer.from(email, 'utf8'), userPda.toBuffer()],
+    anchor.workspace.HousesStake.programId
+  );
+  const adminAccount = getKeypairFromFile('/tests/testAccounts/payer.json');
+
+  const { dataPda, mint: tokenMint } = await getStakeAccounts(
+    email,
+    adminAccount
+  );
+  const stakePdaTokenAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    adminAccount,
+    tokenMint,
+    stakePda,
+    true // allowOwnerOffCurve - allow pda keep tokens
+  );
+  await program.methods
+    .unstake(email, new BN(amount))
+    .accounts({
+      stakeProgram: anchor.workspace.HousesStake.programId,
+      tokenProgram: TOKEN_PROGRAM_ID,
+      systemProgram: anchor.web3.SystemProgram.programId,
+      associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+      tokenMint,
+      userPda,
+      userPdaTokenAccount: userPdaTokenAccount.address,
+      stakePda,
+      stakePdaTokenAccount: stakePdaTokenAccount.address,
+      dataPda,
+      authority: adminAccount.publicKey,
+    })
+    .signers([adminAccount])
+    .rpc();
+};
+
+describe('cpi unstake', () => {
+  const program = anchor.workspace
+    .HousesTokenKeeper as Program<HousesTokenKeeper>;
+
+  anchor.setProvider(anchor.AnchorProvider.env());
+  const adminAccount = getKeypairFromFile('/tests/testAccounts/payer.json');
+  const connection = program.provider.connection;
+  const tokenMint = getLocalMint();
+  const mintAuthority = getKeypairFromFile(
+    '/tests/testAccounts/mintAuthority.json'
+  );
+  const email = getRandomEmail();
+
+  before(async () => {
+    await requestAirdrop(connection, adminAccount.publicKey, 5 * 10e5);
+    const { userPdaTokenAccount } = await getTokenKeeperAccounts(email);
+    await mintTo(
+      connection,
+      adminAccount,
+      tokenMint,
+      userPdaTokenAccount.address,
+      mintAuthority,
+      1000
+    );
+    await cpiStake(email, 500);
+  });
+
+  it('unstake', async () => {
+    const { userPda, userPdaTokenAccount } = await getTokenKeeperAccounts(
+      email
+    );
+    const [stakePda] = PublicKey.findProgramAddressSync(
+      [Buffer.from(email, 'utf8'), userPda.toBuffer()],
+      anchor.workspace.HousesStake.programId
+    );
+
+    const stakePdaTokenAccount = await getOrCreateAssociatedTokenAccount(
+      connection,
+      adminAccount,
+      tokenMint,
+      stakePda,
+      true // allowOwnerOffCurve - allow pda keep tokens
+    );
+
+    assert.equal(await getTokenAmount(userPdaTokenAccount), BigInt(500));
+    await cpiUnstake(email, 300);
+
+    assert.equal(await getTokenAmount(stakePdaTokenAccount), BigInt(200));
+    assert.equal(await getTokenAmount(userPdaTokenAccount), BigInt(800));
+  });
+});
